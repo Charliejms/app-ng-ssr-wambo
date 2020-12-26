ed3a90214614f6bb6f1155dba6ff9ba7f56c8a31
Richard <charlie.jmsklein@gmail.com>
2020-12-26 19:42:10 +0100

    Add refactor shared
    - shared module

--- src/app/wambo/wambo.module.ts
+++ src/app/wambo/wambo.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ManagementComponent } from './components/management/management.component'
 import { HeaderComponent } from './components/header/header.component'
-import { SharedModule } from '../shared/shared/shared.module'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   declarations: [
