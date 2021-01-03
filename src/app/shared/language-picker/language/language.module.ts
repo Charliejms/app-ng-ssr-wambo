9cc0ca82cfd8b9641f1a46be4b14a081e6f25b52
Richard <charlie.jmsklein@gmail.com>
2021-01-03 18:44:06 +0100

    Update language
    - Add module to main component

--- src/app/shared/language-picker/language/language.module.ts
+++ src/app/shared/language-picker/language/language.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { LanguageComponent } from './language.component'
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LanguageComponent,
+  ],
   imports: [
     CommonModule,
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
 import { HttpClient } from '@angular/common/http'
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { LanguagePickerComponent } from './language-picker/language-picker.component'
+import { LanguageModule } from './language-picker/language/language.module'
 
 export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json')
@@ -15,6 +16,7 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
   ],
   imports: [
     CommonModule,
+    LanguageModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
